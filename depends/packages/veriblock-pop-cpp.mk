7f68945056ef9d5e397f52f9be4a3419f1ec5ad6
Bohdan <warchantua@gmail.com>
2020-08-21 16:32:35 +0300

    Fix pop fr bug (#318)

--- depends/packages/veriblock-pop-cpp.mk
+++ depends/packages/veriblock-pop-cpp.mk
@@ -1,8 +1,8 @@
 package=veriblock-pop-cpp
-$(package)_version=4e3f5508ca229cb36921488877002668ae2b3378
+$(package)_version=1451bbd6763873229c972a299f28eaa007c81348
 $(package)_download_path=https://github.com/VeriBlock/alt-integration-cpp/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=fc6fe7a6a58e2da476e5a9d730066a661c9995af9ee8462a896396c247631572
+$(package)_sha256_hash=998d3a4c7126fe3b662fc20cc8086c656d23bb8b9b29eab53685838004b3b92f
 $(package)_build_subdir=build
 $(package)_build_type=$(BUILD_TYPE)
 $(package)_asan=$(ASAN)
